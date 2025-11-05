46be1926ee16dab31dd989c70f766248a5fce7d6
UKSTRYKER <90646675+UKSTRYKER@users.noreply.github.com>
2025-11-04 17:04:59 -0700

    Clean up using directives and comments in Program.cs
    Removed unused using directives and comments.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
 namespace Inventory_Management_System
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
